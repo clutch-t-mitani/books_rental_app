53c16c6ca2076a2b934fc8228ecde45c98dc1b86
三谷朋之 <tomoyuki.mitani@clutch-eng.com>
2023-01-30 11:45:28 +0900

    不要コード削除

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -70,8 +70,5 @@
         }
         document.deleteform.submit();
     };
-    window.addEventListener('pageshow',()=>{
-	    if(window.performance.navigation.type==2) location.reload();
-    });
 </script>
 
